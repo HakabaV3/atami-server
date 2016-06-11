d5f4247d8a3a437de1af5e86fe739140ae8a096b
kikura <y.kikura@gmail.com>
2016-06-11 20:38:55 +0900

    #57 プロキシを有効化

--- src/routes/v1/image.ts
+++ src/routes/v1/image.ts
@@ -1,7 +1,20 @@
 import {Model} from "../../models/model"
 import {Image} from "../../models/image"
 import * as express from 'express'
+import * as request from 'request'
 export const router = express.Router()
+
+router.get('/proxy/:id', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+    console.log("!");
+    Image.pFindById(req.param('id'))
+        .then((image: Image) => {
+            console.log(image.constructor);
+            request.get(image.originalUrl).pipe(res);
+        })
+        .catch(err => {
+            res.sendStatus(404);
+        });
+});
 
 router.get('/:id', function(req: express.Request, res: express.Response, next: express.NextFunction) {
     console.log(req.param('id'));
@@ -18,6 +31,9 @@ router.post('/', function(req: express.Request, res: express.Response, next: exp
     let image = new Image();
     image.originalUrl = req.body.url;
     image.tags = req.body.tags;
+
+    //@TODO ハードコードなくす
+    image.proxiedUrl = `https://amp.kikurage.xyz/v1/image/proxy/${image.id}`;
 
     image.pSave()
         .then(() => {
