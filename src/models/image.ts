ad92b5330ea82e0daec4bec688d4319e7ee4ada4
kikura <y.kikura@gmail.com>
2016-06-11 20:58:56 +0900

    #61 タグ機能の実装

--- src/models/image.ts
+++ src/models/image.ts
@@ -1,18 +1,17 @@
 import {Model, UUID} from "./model"
-import {Tag} from "./tag"
 
 export class Image extends Model {
     id: UUID
     originalUrl: string
     proxiedUrl: string
-    tags: Tag[]
+    tags: string[]
 
     downgrade() {
         return {
             id: this.id,
             originalUrl: this.originalUrl,
             proxiedUrl: this.proxiedUrl,
-            tags: this.tags.map(tag => tag.text)
+            tags: this.tags
         }
     }
 }
